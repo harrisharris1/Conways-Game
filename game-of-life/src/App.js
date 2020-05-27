ccc74b2a63b82f2ff3fe560b190c4fa6ac264dff
Andrea Harris <andrea@andreas-imac.lan>
2020-05-27 14:51:48 -0500

    bug in grid i cant fix yet

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -5,31 +5,51 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      size: [70, 30],
+      size: [30, 5],
     }
-
-    this.handleColumnChange = this.handleColumnChange.bind(this);
     this.handleRowChange = this.handleRowChange.bind(this);
+    this.handleColumnChange = this.handleColumnChange.bind(this);
     this.startGame = this.startGame.bind(this);
     this.stopGame = this.stopGame.bind(this);
     this.renderCanvas = this.renderCanvas.bind(this);
   }
-
   handleRowChange(event) {
+    if (!this.state.gameIsRunning){
+      var recentSize =this.state.size;
+
+      if(event.target.value < 30)
+      recentSize[0] =event.target.value;
+      else
+      recentSize[0]= 30;
+
+      this.setState({
+        size: recentSize,
+      });
+
+      this.renderCanvas();
+    }
   }
+
+  
 
   handleColumnChange(event) {
+    if(!this.state.gameIsRunning){
+      var recentSize =this.state.size;
+      if (event.target.value < 5)
+      recentSize[0] = event.taget.value;
+      else
+      recentSize[0] = 5;
+
+      this.setState({
+        size: recentSize,
+      });
+
+      this.renderCanvas();
+    }
+
   }
 
-  startGame() {
-  }
-
-  stopGame(){
-  }
-
-  runGame() {
-  }
-
+  
   renderCanvas() {
     var newCanvas = [];
     var rowCells = [];
@@ -45,7 +65,30 @@ export default class App extends Component {
     return newCanvas;
 
   }
+  startGame() {
+    if(!this.state.gameIsRunning){
+      this.setState({
+        gameIsRunning: true,
+      }, () => {
+        this.intervalRef = setInterval(() => this.runGame(), 10);
+      })
+    }
+  }
 
+  stopGame(){
+    this.setState({
+      gameIsRunning: false
+    }, () => {
+      if(this.intervalRef) {
+        clearInterval(this.intervalRef);
+      }
+    })
+  }
+
+  runGame() {
+  }
+
+  
   render() {
     return (
       <div className="container">
@@ -59,9 +102,17 @@ export default class App extends Component {
                 {this.renderCanvas()}
                 </div>
           </div> 
+          <div className="rules">
+          <ul>
+            <li>Any live cell with fewer than two live neighbours dies, as if by underpopulation.</li>
+            <li>Any live cell with two or three live neighbours lives on to the next generation.</li>
+            <li>Any live cell with more than three live neighbours dies, as if by overpopulation.</li>
+            <li>Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.</li>
+          </ul>
+        </div>
           <label className="label">
                 Columns:
-                <input className="input" type="text" value={this.state.size[0]} onChange={this.handleColumnChange} />
+                <input className="input" type="text" value={this.state.size[1]} onChange={this.handleColumnChange} />
               </label>
 
             <label className="label">
@@ -81,7 +132,8 @@ export default class App extends Component {
 class Cell extends Component {
   render() {
     return (
-      <div className="cell-container"></div>
+      <div className="cell-container">
+      </div>
     );
   }
 }
